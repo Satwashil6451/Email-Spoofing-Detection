d69db21466056f45f979f0486e50383c1a920935
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2026-03-31 21:23:50 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -14,6 +14,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+
 import joblib
 
 def train(path_csv="data/features.csv", out_model="models/rf_spoof.pkl"):

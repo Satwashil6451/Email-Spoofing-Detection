f64ff01636c6776fcd5f858ae3c5aa6d28e2bfd2
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2026-04-20 19:20:59 +0530

    Update features.py

--- src/features.py
+++ src/features.py
@@ -18,6 +18,7 @@ def domain_of_url(url: str) -> str:
         # Extract and return the domain (netloc), convert to lowercase and remove any accidental spaces.
         # Example: https://paypal.com/login → paypal.com
         return p.netloc.lower().strip()
+
     except Exception:
         # If URL parsing fails, return empty string
         return ""
